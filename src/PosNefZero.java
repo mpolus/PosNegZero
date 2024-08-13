5215ac5b6b7e9506b467ca4bf03f77de7b6c208b
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-13 13:31:57 -0400

    la

--- src/PosNefZero.java
+++ src/PosNefZero.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class PosNefZero {
+public class PosNefZero {
+    public static void main(String[] args) {
+        checkNumber(2);
+        checkNumber(-3);
+        checkNumber(0);
+
+    }
+    public static void checkNumber(int number){
+        if (number > 0){
+            System.out.println("The number is positive.");
+        } else if(number < 0){
+            System.out.println("The number is negative.");
+        } else{
+            System.out.println("The number is 0.");
+        }
+    }
 }
